0ba3003647943a76ea112bb66106d3e6a72875dd
kalush666 <jonathan.kalush@gmail.com>
2025-06-03 13:17:41 +0300

    student dto

--- dto/StudentDTO.java
+++ dto/StudentDTO.java
@@ -1,7 +1,32 @@
 package kalush666.studentmanagment.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentDTO {
+    private Long id;
+
+    @NotBlank(message = "First name is mandatory")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is mandatory")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    private String lastName;
+
+    @Min(value = 0, message = "Age must be a positive number")
+    private Double age;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
 }
