1890b36438bbeb85eed1638bced40ef6d9804da9
kalush666 <jonathan.kalush@gmail.com>
2025-06-03 13:10:50 +0300

    struct

---
+++ student_managment/StudentManagmentApplication.java
@@ -0,0 +1,13 @@
+package kalush666.studentmanagment.student_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagmentApplication.class, args);
+    }
+
+}
